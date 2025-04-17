cdcba751fb81abf3ef582a25fe44999c91e52a45
sangyup <ideallyup@naver.com>
2025-04-17 19:55:37 +0900

    :sparkles:: Delegation
    Delegation을 활용한 데이터 전달

---
+++ SOPT_week2/SOPT_week2/SOPT_week2/Delegate/LoginViewController_Delegate.swift
@@ -0,0 +1,107 @@
+//
+//  LoginViewController_Delegate.swift
+//  SOPT_week2
+//
+//  Created by 이상엽 on 4/12/25.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController_Delegate: UIViewController, DataBindDelegate {
+    func dataBind(id: String) {
+        idTextField.text = id
+    }
+    
+    let titleLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 69, y :161, width: 240, height: 44))
+        label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = UIFont(name: "Pretendard-Bold", size: 18)
+        label.lineBreakStrategy = .hangulWordPriority
+        return label
+    }()
+    
+    let idTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
+        textField.placeholder = "아이디"
+        textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+        textField.becomeFirstResponder()
+        textField.borderStyle = .roundedRect
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.backgroundColor = UIColor(named: "gray200")
+        return textField
+    }()
+    let passwordTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
+        textField.placeholder = "비밀번호"
+        textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = UIColor(named: "gray200")
+        return textField
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
+        button.backgroundColor = UIColor(named: "orange_carrot")
+        button.setTitle("로그인하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 6
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.1
+        button.layer.shadowOffset = CGSize(width: 0, height: 4)
+        button.layer.shadowRadius = 6
+        button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 14)
+        button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    
+    private func presentToWelcomVC() {
+        let welcomeViewController = WelcomViewController_Delegate()
+        welcomeViewController.modalPresentationStyle = .formSheet
+//        welcomeViewController.id = idTextField.text
+        welcomeViewController.delegate = self
+        welcomeViewController.setLabelText(id: idTextField.text)
+        self.present(welcomeViewController, animated: true)
+    }
+    
+    private func pushToWelcomVC() {
+        let welcomeViewController = WelcomViewController_Delegate()
+//        welcomeViewController.id = idTextField.text
+        welcomeViewController.delegate = self
+        welcomeViewController.setLabelText(id: idTextField.text)
+        self.navigationController?.pushViewController(welcomeViewController, animated: true)
+        
+    }
+    
+    private func textFiedlShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc
+    func loginButtonDidTap() {
+        presentToWelcomVC()
+//        pushToWelcomVC()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(idTextField)
+        self.view.addSubview(passwordTextField)
+        self.view.addSubview(loginButton)
+//        textFiedlShouldReturn(idTextField)
+
+    }
+}
